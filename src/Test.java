6881c0943f2a6d6c2e221b7a44666a81878610d4
instinct <709837726@qq.com>
2021-07-06 21:57:36 +0800

    commit test

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List heros = new ArrayList();
+        heros.add("me");
+        heros.add("amd yes");
+        System.out.println(heros.size());
+
+    }
+
+
+    // https://github.com/tianhua272/ars.git
+}
